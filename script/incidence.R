805dfd7ac1b7588b063beb7ed104e3a1d2174937
Deus <d.thindwa@imperial.ac.uk>
2021-07-28 17:52:29 +0200

    mlw

--- script/incidence.R
+++ script/incidence.R
@@ -3,7 +3,6 @@
 # exponential decay and growth models.
 # 1/08/2021-30/09/2021
 
-
 # load the IPD cases
 ipd <- readr::read_csv(here("data", "ipd_incid_EW.csv"))
 ipd <- dplyr::mutate(ipd, agey = readr::parse_number(substr(agegroup, 1, 2)))
@@ -17,12 +16,19 @@ beta0  <- coef(model0)[2]
 
 # fit nonlinear (weighted) least-squares estimates of the parameters using Gauss-Newton algorithm
 # we parameterise in terms of log-rates to ensure the estimates are positive
-ipd_models <- ipd %>% 
+ipd_modelEW <- filter(ipd, country == "Englad/Wales") %>% 
     split(.$serogroup) %>%
     purrr::map(~nls(data = .x, 
                     incidence ~ exp(log_alpha) * exp(beta*agey) + (theta),
                     nls.control(maxiter = 200),
                     start = list(log_alpha = (alpha0), beta  = (beta0), theta = (theta0))))
+
+ipd_modelMW <- filter(ipd, country == "Malawi") %>% 
+  split(.$serogroup) %>%
+  purrr::map(~nls(data = .x, 
+                  incidence ~ exp(log_alpha) * exp(beta*agey) + (theta),
+                  nls.control(maxiter = 200),
+                  start = list(log_alpha = (alpha0), beta  = (beta0), theta = (theta0))))
 
 ipd_x <- data.frame(agey = seq(55, 90, by = 1))
 
@@ -50,19 +56,64 @@ summarise_from_model <- function(x, probs = c(0.025, 0.5, 0.975)){
 }
 
 # run the simulations
-ipd_mc <- ipd_models %>% map(~simulate_from_model(.x, newdata = ipd_x, nsim = Nsims))
+ipd_mcEW <- ipd_modelEW %>% map(~simulate_from_model(.x, newdata = ipd_x, nsim = Nsims))
+ipd_mcMW <- ipd_modelMW %>% map(~simulate_from_model(.x, newdata = ipd_x, nsim = Nsims))
 
 # summarise uncertainty
-ipd_curves <- ipd_mc %>%
-    map_df(summarise_from_model, .id = "serogroup") %>%
-    mutate(serogroup = fct_inorder(factor(serogroup))) 
+ipd_curvesEW <- ipd_mcEW %>% map_df(summarise_from_model, .id = "serogroup") %>% mutate(serogroup = fct_inorder(factor(serogroup)), country = "Englad/Wales")
+ipd_curvesMW <- ipd_mcMW %>% map_df(summarise_from_model, .id = "serogroup") %>% mutate(serogroup = fct_inorder(factor(serogroup)), country = "Malawi") 
 
 # calculate scaled incidence
-ipd_scaled <- ipd %>%
-    dplyr::group_by(serogroup) %>%
-    dplyr::mutate(p = incidence/sum(incidence))
+ipd_scaled <- ipd %>% dplyr::group_by(serogroup) %>% dplyr::mutate(p = incidence/sum(incidence))
 
 # generate IPD cases from total pop and IPD incidence annually
-Cases <- dplyr::inner_join(bind_rows(ipd_mc, .id="serogroup"), countries_df, by = "agey") %>%
-    dplyr::filter(serogroup != "All serotypes") %>%
-    dplyr::mutate(cases = fit/1e5*ntotal, Vac.age = agey)
+CasesEW <- dplyr::inner_join(bind_rows(ipd_mcEW, .id="serogroup"), countries_df, by = "agey") %>% dplyr::filter(serogroup != "All serotypes") %>% dplyr::mutate(cases = fit/1e5*ntotal, Vac.age = agey)
+CasesMW <- dplyr::inner_join(bind_rows(ipd_mcMW, .id="serogroup"), countries_df, by = "agey") %>% dplyr::filter(serogroup != "All serotypes") %>% dplyr::mutate(cases = fit/1e5*ntotal, Vac.age = agey)
+
+# plot backward or forward extrapolation incidence
+incidence_plot <-  
+  ggplot(data = rbind(ipd_curvesEW, ipd_curvesMW), aes(x = agey, y = `50%`, color = serogroup, fill  = serogroup)) +
+  geom_line() +
+  geom_ribbon(aes(ymin = `2.5%`, ymax = `97.5%`), alpha = 0.2, color = NA) +
+  facet_grid(serogroup ~ country) +
+  ylim(c(0, NA)) + 
+  theme_bw() +
+  xlab("Age (years)") +
+  ylab("Incidence (cases per 100,000)") +
+  theme(legend.position = "bottom") +
+  scale_color_brewer(palette = "Dark2") +
+  scale_fill_brewer(palette = "Dark2") +
+  geom_point(data = ipd, aes(y = incidence))
+
+ggsave(here("output","incidence_plot.png"),
+       plot = incidence_plot,
+       width = 7, height = 5, unit="in", dpi = 300)
+
+
+#scaled incidence plot
+scaled_incidence_plot <-
+  ggplot(data = ipd_scaled, aes(x = agey, y = p, color = serogroup)) + 
+  geom_line() +
+  theme_bw() +
+  xlab("Age (years)") +
+  ylab("Observed incidence") +
+  ylim(c(0, NA)) +
+  scale_color_brewer(palette = "Dark2", guide = FALSE)
+
+
+ggsave(here("output","scaled_plot.png"),
+       plot = scaled_incidence_plot,
+       width = 7, height = 5, unit="in", dpi = 300)
+
+
+incidence_plots <- 
+  scaled_incidence_plot +
+  incidence_plot +
+  patchwork::plot_layout(nrow = 1,
+                         guides = "collect") &
+  theme(legend.position='bottom')
+
+ggsave(here("output", "incidence_plots.png"),
+       plot = incidence_plots,
+       width = 7, height = 4, unit="in", dpi = 300)
+
