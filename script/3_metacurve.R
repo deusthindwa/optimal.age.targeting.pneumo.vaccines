b1a50906c490a2763ee9250a7862e552ebc31a59
Sam Clifford <sj.clifford@gmail.com>
2022-01-31 21:15:01 +0000

    ease of reading

--- script/3_metacurve.R
+++ script/3_metacurve.R
@@ -5,27 +5,28 @@
 
 # vaccine efficacy from published papers
 dat <- list(`Andrews et al. (2012)` = list(`0-2` = c(48, 32, 60),
-                                    `2-5` = c(21, 03, 06),
-                                    `5-Inf` = c(15, -3, 30)),
+                                           `2-5` = c(21, 03, 06),
+                                           `5-Inf` = c(15, -3, 30)),
             
             `Wright et al. (2013)` = list(`0-5` = c(-9, -119, 43),
-                                   `5-10` = c(38.3, -6, 64),
-                                   `10-Inf` = c(-21, -137, 35)),
+                                          `5-10` = c(38.3, -6, 64),
+                                          `10-Inf` = c(-21, -137, 35)),
             
             `Rudnick et al. (2013)` = list(`0-5` = c(41.3, 20, 57),
-                                    `5-Inf` = c(34.1, 6, 54)),
+                                           `5-Inf` = c(34.1, 6, 54)),
             
             `Guttierez et al. (2014)` = list(`0-5` = c(44.5, 19, 62),
-                                      `5-Inf` = c(32.5, -6, 57)),
+                                             `5-Inf` = c(32.5, -6, 57)),
             
             `Djennad et al. (2018)` = list(`0-2` = c(41, 23, 54),
-                                    `2-5` = c(34, 16, 48),
-                                    `5-Inf` = c(23, 12, 32)))
+                                           `2-5` = c(34, 16, 48),
+                                           `5-Inf` = c(23, 12, 32)))
 
 # function to create dataset for published VE excluding Wright's paper
-dat_ <- lapply(X = dat, FUN = function(x){
-  map_df(x, ~set_names(.x, c("Mean", "Min", "Max")), .id = "Ages")
-  }) %>%
+dat_ <- lapply(X = dat, 
+               FUN = function(x){
+                 map_df(x, ~set_names(.x, c("Mean", "Min", "Max")), .id = "Ages")
+               }) %>%
   bind_rows(.id = "Study") %>%
   separate(Ages, into = c("xmin", "xmax")) %>%
   mutate_at(.vars = vars(xmin, xmax), .funs = parse_number) %>%
